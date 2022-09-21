d31b825bb885c77a0c0f99ad36afe5b7754c345a
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-09-21 17:08:31 +0300

    react runtime color
    react runtime color app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import Palette from './components/Palette';
+import { useEffect, useState } from 'react';
+import { init, subscribe } from './socketApi';
+
+
+function App() {
+  const [activeColor, setActiveColor] = useState("#282c34");
+
+  useEffect(() =>{
+    init();
+
+    subscribe((color) =>{
+      setActiveColor(color);
+    });
+  }, [])
+
+  return (
+    <div className="App" style={{backgroundColor: activeColor}}>
+      <Palette activeColor={activeColor}/>
+    </div>
+  );
+}
+
+export default App;
